732f02b7d787124244653d8a90659c9c82ae796c
Nandish Mohanty <78742833+NANDISH-18@users.noreply.github.com>
2023-11-02 00:03:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from './pages/home/Home';
 import  Details  from './pages/details/Details'
 import  SearchResult  from './pages/searchResult/SearchResult'
 import  Explore  from './pages/explore/Explore'
-import PageNotFound from './pages/404/pageNotfound'
+import PageNotFound from './pages/404/PageNotfound'
 
 
 function App() {
